f0d95d7770bcdb013f5e7b9a88a4cff98c076ccc
Thai <tnguy187@jhu.edu>
2017-04-12 13:07:30 -0400

    Viewflipper and animations

--- app/src/main/java/com/example/thai/ui20/day_calendar.java
+++ app/src/main/java/com/example/thai/ui20/day_calendar.java
@@ -3,15 +3,68 @@ package com.example.thai.ui20;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ViewFlipper;
 
 public class day_calendar extends WearableActivity {
 
+    ViewFlipper viewFlipper;
+    private float initialX;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_calendar);
+
+        viewFlipper = (ViewFlipper) this.findViewById(R.id.viewFlipper);
+        //viewFlipper.addView();
+        //viewFlipper.setOnClickListener(this);
+
     }
+    public void onButtonDown (View v){
+        viewFlipper.setInAnimation(this, R.anim.low_to_high_enter);
+        viewFlipper.setOutAnimation(this,R.anim.low_to_high_out);
+        viewFlipper.showNext();
+    }
+
+    public void onButtonUp (View v){
+        viewFlipper.setInAnimation(this, R.anim.high_to_low_enter);
+        viewFlipper.setOutAnimation(this,R.anim.high_to_low_out);
+        viewFlipper.showPrevious();
+    }
+
+/*
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                // Getting initial by event action down
+                initialX = event.getY();
+                break;
+
+            case MotionEvent.ACTION_UP:
+
+                // On action up the flipper will start and showing next item
+                float finalX = event.getY();
+                if (initialX > finalX) {
+
+                    // Show items are 4
+                    if (viewFlipper.getDisplayedChild() == 2)
+                        break;
+
+                    // Flip show next will show next item
+                    viewFlipper.showNext();
+                } else {
+
+                    // If flip has no items more then it will display previous item
+                    if (viewFlipper.getDisplayedChild() == 0)
+                        break;
+                    viewFlipper.showPrevious();
+                }
+                break;
+        }
+        return false;
+    }
+*/
 
     public void backToMenu(View v) {
         Intent openMainActivity = new Intent(day_calendar.this, MainActivity.class);
@@ -20,4 +73,6 @@ public class day_calendar extends WearableActivity {
 
         finish();
     }
+
+
 }
--- app/src/main/java/com/example/thai/ui20/press_and_hold.java
+++ app/src/main/java/com/example/thai/ui20/press_and_hold.java
@@ -1,29 +1,43 @@
 package com.example.thai.ui20;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.os.Vibrator;
 
-public class press_and_hold extends WearableActivity {
+public class press_and_hold extends WearableActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_press_and_hold);
+        final ImageView hold_button = (ImageView) findViewById(R.id.hold_button);
+        hold_button.setOnLongClickListener(new View.OnLongClickListener(){
+            @Override
+            public boolean onLongClick(View v) {
+                Vibrator vibrate_button = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibrate_button.vibrate(100);
+                Intent intent = new Intent(press_and_hold.this, day_calendar.class);
+                startActivity(intent);
+                return false;
+            }
+        });
     }
 
     public void backToMenu(View v) {
+
         Intent openMainActivity = new Intent(press_and_hold.this, MainActivity.class);
         openMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(openMainActivity);
-
         finish();
     }
 
-    public void day_calendar(View v) {
+/*    public void day_calendar(View v) {
         Intent intent = new Intent(this, day_calendar.class);
         startActivity(intent);
 
-    }
+    }*/
 }
